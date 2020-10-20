c5e572330504b63dbf24db409ab6c6235467e32e
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-20 18:11:58 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,7 +9,12 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  
+  new_cart = []
+  counter = 0 
+  while counter < cart.length 
+    
+    counter += 1 
+  end
 end
 
 
