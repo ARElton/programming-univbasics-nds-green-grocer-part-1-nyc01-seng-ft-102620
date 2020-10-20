889a5f040f4cd9df952a355927cf10d6a9c91340
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-20 18:05:10 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,7 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   counter = 0 
   while counter < collection.length 
-    if collection[counter][:name] == name
+    if collection[counter][:item] == name
       return collection[counter]
     end
     counter += 1 
